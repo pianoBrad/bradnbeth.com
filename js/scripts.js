e6578546bc9b919b76c9704f98ab0b026d9fd0d8
Brad Phillips <bradphillips@fabricatorz.com>
2014-02-02 22:06:20 -0600

    Add a second slide & give Brad a falling animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+var slide_transition = function(index, direction) {
+	if ( index == 1 && direction == 'down' ) {
+		console.log('going down!!');
+		$('.brad .fixed').attr('src','images/brad-falling.svg');
+	} else if ( index == 2 && direction == 'up') {
+		console.log('heading back up!!');
+		$('.brad .fixed').attr('src','images/brad-falling.svg');
+	}
+}
+
+var slide_loaded = function(anchorLink, index) {
+	if ( index == 2 || index == 1 ) {
+		$('.brad .fixed').attr('src','images/brad.svg')
+	}
+}
+
+
 $(document).ready(function() {
-	$.fn.fullpage();
+	$.fn.fullpage({
+		afterLoad: function(anchorLink, index){
+			slide_loaded(anchorLink, index);
+		},
+		onLeave: function(index, direction){
+			slide_transition(index, direction);
+		}
+	});
 });
